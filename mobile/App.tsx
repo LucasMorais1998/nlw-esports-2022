92fca1ca269570eef3d0270ca110d54a988cb2dd
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-17 19:39:33 -0300

    mobile: feat App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,33 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Button title="Send 1" />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
   );
 }
-
-interface IButtonProps {
-  title: string;
-}
-
-function Button(props: IButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
