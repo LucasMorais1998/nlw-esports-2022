39b7b8b3f6a733bb85650ba6bf81ae7d913946f3
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-17 16:05:20 -0300

    server: clean file

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/ads", (request, response) => {
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-  ]);
+  return response.json({
+    hello: "world",
+  });
 });
 
 app.listen(3333);
