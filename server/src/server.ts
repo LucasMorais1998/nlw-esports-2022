33588500822713c5de6c0577fb37685ec395853b
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-18 16:56:44 -0300

    server: create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,21 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  return response.json({
+    hello: "world",
+  });
+});
+
+app.get("/ads/:id/discord", (request, response) => {
   return response.json({
     hello: "world",
   });
