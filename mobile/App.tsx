52405b428f051aff871a5303ebeb42e684e64b7c
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-17 22:30:56 -0300

    mobile: feat App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,24 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
 import { StatusBar } from "react-native";
+
 import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
     <Background>
       <StatusBar
@@ -9,6 +26,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
